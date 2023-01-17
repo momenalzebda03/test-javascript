7a0347a79aab82b93ec565a0f0b3c8f640c8b59b
unknown <momen07996@gmail.com>
2023-01-17 00:48:56 -0800

    js the emit 10 or 17

--- indx.js
+++ indx.js
@@ -27,5 +27,39 @@ console.table(["elzero", "ahmed", "sameh", "gamal", "aya"]);
 // ---------------------------
 console.log("Iam In Console");
 document.write("Iam In Page");
-alert("stop");
+// alert("stop");
 // end 9
+// start 10
+var my_title = "elzero", my_description = "elzero web school", my_date = "25/10";
+var my_html = `<div class = 'card'>
+
+    <h1>"${my_title}"</h1>
+    <p>"${my_description}"</p>
+    <span>"${my_date}"</span>
+</div>`;
+
+for (let i = 0; i <= 3; i++) {
+    document.write(my_html);
+}
+
+var number_one = 10;
+var number_tow = 20;
+
+console.log(`${number_one}${number_tow}`);
+console.log(typeof number_one + typeof number_tow);
+console.log(`${number_one}${number_tow}`);
+console.log(typeof number_one + typeof number_tow);
+console.log(number_tow + "\n" + number_one);
+console.log(`${number_tow}
+${number_one}`);
+
+console.log(elzero.innerHTML); // object
+console.log(typeof elzero); // object
+
+console.log(`\`I'm In \n\\\\\nLove \\\\ """ '''\n++ With ++\n\\"""\\"""\n""javascript""\`\``);
+
+let a = 21;
+let b = 20;
+
+console.log(`_${a}_${b}${a}_${b}${a}_${b}${a}_${b}_`); // _21_2021_2021_2021_20_
+// end 17
